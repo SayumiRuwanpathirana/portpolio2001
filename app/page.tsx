e2a90d0eda47865a94581ef9fadfb40236bb8d4f
Sayumi Tharumila Ruwanpathirana <144215864+SayumiRuwanpathirana@users.noreply.github.com>
2024-04-19 00:38:15 +0530

    edit these codes

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import AboutSection from "@/components/AboutSection"
 import HeroSection from "@/components/HeroSection"
 import ProjectsSection from "@/components/ProjectsSection"
 
-useClient();
+
 
 export default function Home() {
   return (
--- components/ProjectsSection.tsx
+++ components/ProjectsSection.tsx
@@ -4,7 +4,7 @@ import Link from "next/link"
 import SlideUp from "./SlideUp"
 import { BsGithub, BsArrowUpRightSquare } from "react-icons/bs"
 
-useClient();
+
 
 const projects = [
   {
